fee680682f8793d2d0bc471d0493aef8a68ecdfe
vagrant <vagrant@homestead>
2018-08-02 09:29:57 +0000

    botton

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -79,7 +79,7 @@
                 <div class="avatar"><img src="{{ asset('/images/avatar.jpg') }}" alt=""></div>
                 <p class="abname">网名 ：这小样谁对象</p>
                 <p class="abname">Q Q ：358282005&nbsp</p>
-                <p class="abname">邮箱 ：zhangzhen@yeah.net</p>
+                <p class="abname">邮箱 ：358282005@qq.com</p>
                 <div class="abtext"> 一个擦边90后！2012年07毕业于大连工业大学，而后当了一名电工。2013年转行做了一名程序员。 </div>
             </div>
         </div>
--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,23 @@
-<footer class="footer">
-    <div class="footer-inner">
-        <div class="copyright pull-left"> Copyright &copy; 2002-2011 DEDE58.COM 织梦模板 版权所有 </div>
-        <div class="trackcode pull-right">
+<footer class="ft">
+    <div class="ft_top">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <dl>
+                        <dt>联系方式</dt>
+                        <dd>QQ:358282005</dd>
+                        <dd>Email:358282005@qq.com</dd>
+                    </dl>
+                </div>
+                <div class="col-sm-8 ft_text">"人生当自勉，学习需坚持。从这一开始，我依旧是我，只是心境在不同。不论今后的路如何，我都在心底默默鼓励自己，坚持不懈，等待那一刻破茧的美丽"</div>
+            </div>
+        </div>
+    </div>
+    <div class="ft_bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">Copyright © 2016 imooc.com All Rights Reserved | 京ICP备16031026号-1</div>
+            </div>
+        </div>
     </div>
 </footer>
