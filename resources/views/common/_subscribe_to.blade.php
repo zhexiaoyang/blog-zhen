fc9a3d28c618ed0382c0934047ee19bd2d3e6d06
“zhangzhen <zhangzhen@yeah.net>
2018-11-23 16:10:53 +0800

    laravel-admin add https

--- resources/views/common/_subscribe_to.blade.php
+++ resources/views/common/_subscribe_to.blade.php
@@ -2,14 +2,14 @@
     <div class="title">
         <h2> 邮件订阅 </h2>
     </div>
-    <form action="http://list.qq.com/cgi-bin/qf_compose_send" target="_blank"
-          method="post">
-        <p> 填写您的邮件地址，订阅更多的精彩内容： </p>
-        <input name="t" value="qf_booked_feedback" type="hidden">
-        <input name="id" value="d424e09a19a79d14ee16b72b7fe86673a74b547ae602c157"
-               type="hidden">
-        <input name="to" class="rsstxt" placeholder="your@email.com" required=""
-               type="email">
-        <input class="rssbutton" value="订阅" type="submit">
-    </form>
+    {{--<form action="http://list.qq.com/cgi-bin/qf_compose_send" target="_blank"--}}
+          {{--method="post">--}}
+        {{--<p> 填写您的邮件地址，订阅更多的精彩内容： </p>--}}
+        {{--<input name="t" value="qf_booked_feedback" type="hidden">--}}
+        {{--<input name="id" value="d424e09a19a79d14ee16b72b7fe86673a74b547ae602c157"--}}
+               {{--type="hidden">--}}
+        {{--<input name="to" class="rsstxt" placeholder="your@email.com" required=""--}}
+               {{--type="email">--}}
+        {{--<input class="rssbutton" value="订阅" type="submit">--}}
+    {{--</form>--}}
 </div>
